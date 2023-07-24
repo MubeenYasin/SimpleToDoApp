d076f44f48bb3e2bbeca4ed31e6446225b4e51f1
MubeenYasin <mubeen.yasin@gmail.com>
2023-07-24 21:20:53 +0300

    input component

--- components/Input.js
+++ components/Input.js
@@ -1,13 +1,35 @@
-import { TextInput } from "react-native";
-
-function InputText(inputext){
-    console.log(inputext)
-}
+import React, { useState } from "react";
+import { TextInput, View, Text, Button } from "react-native";
 
 const Input = () => {
-    return (  
-        <TextInput placeholder='Add a task' onChangeText={InputText} />
+
+    const [inpuText, setInpuText] = useState('')
+    const [task, setTask] = useState([''])
+
+    // function InputedText(inputext) {
+    //     // console.log(inputext)
+    //     setInpuText(inputext)
+    // }
+    const InputedText = (inputext) => {
+        setInpuText(inputext)
+    }
+
+    // function AddTask() {
+    //     setTask([...task, inpuText])
+    // }
+    const AddTask = () => {
+        setTask([...task, inpuText])
+    }
+
+    return (
+        <View>
+            <TextInput placeholder='Add a task' onChangeText={InputedText} />
+            <Button title="add task" onPress={AddTask} />
+            {task.map((item) => <Text key={item}>{item}</Text>)}
+
+            
+        </View>
     );
 }
- 
+
 export default Input;
